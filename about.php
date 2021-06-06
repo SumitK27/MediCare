09cd27208fbb4b55fe41d1f9c33db10d5cc0e5dd
SumitK27 <sumitkajbaje@gmail.com>
2021-06-06 17:30:06 +0530

    Navbar bug Fixes

--- about.php
+++ about.php
@@ -2,6 +2,9 @@
 require_once('./includes/imports.php');
 require_once('./includes/config.php');
 require_once('./includes/classes/Account.php');
+$account = new Account($conn);
+$userInfo = $account->getInfo();
 require_once('./includes/components/navbar.php');
+require_once('./includes/importsAfter.php');
 ?>
 <title>About</title>
--- contact.php
+++ contact.php
@@ -2,7 +2,10 @@
 require_once('./includes/imports.php');
 require_once('./includes/config.php');
 require_once('./includes/classes/Account.php');
+$account = new Account($conn);
+$userInfo = $account->getInfo();
 require_once('./includes/components/navbar.php');
+require_once('./includes/importsAfter.php');
 ?>
 
 <title>Contact Us</title>
